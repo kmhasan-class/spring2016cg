8513316976d9b977bd28643ca7b7860531e51e44
kmhasan <kmhasan@SEUPC0719>
2016-03-28 14:38:08 +0600

    First LWJGL program -- lab task

--- Hello LWJGL/src/hello/lwjgl/HelloLWJGL.java
+++ Hello LWJGL/src/hello/lwjgl/HelloLWJGL.java
@@ -43,6 +43,18 @@ public class HelloLWJGL {
 
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
+        // draw the dial
+        GL11.glPushMatrix();
+        GL11.glBegin(GL11.GL_LINES);
+        int n = 100;
+        float dtheta = (float) (2 * Math.PI / n);
+        for (int i = 0; i < n; i++) {
+            GL11.glVertex3f((float) (1 * Math.cos(i * dtheta)), (float) (1 * Math.sin(i * dtheta)), 0f);
+            GL11.glVertex3f((float) (1 * Math.cos((i + 1) * dtheta)), (float) (1 * Math.sin((i + 1) * dtheta)), 0f);
+        }
+        GL11.glEnd();
+        GL11.glPopMatrix();
+
         while (true) {
             double t = GLFW.glfwGetTime();
 
