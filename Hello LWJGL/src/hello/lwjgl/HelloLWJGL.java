1adb9e0c2fc08252708a380c97b5366fafb67708
kmhasan <kmhasan@SEUPC0719>
2016-03-28 13:03:35 +0600

    First LWJGL program -- lab task

--- Hello LWJGL/src/hello/lwjgl/HelloLWJGL.java
+++ Hello LWJGL/src/hello/lwjgl/HelloLWJGL.java
@@ -6,7 +6,9 @@
 package hello.lwjgl;
 
 import org.lwjgl.Version;
-
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 
 /**
  *
@@ -14,16 +16,41 @@ import org.lwjgl.Version;
  */
 public class HelloLWJGL {
 
+    private int WIDTH = 800;
+    private int HEIGHT = 600;
+    private long window;
+
     public HelloLWJGL() {
-        System.out.println("Hello everyone!");
-        System.out.println("LWJGL version " + Version.getVersion());
+        init();
+        loop();
     }
-    
+
+    private void init() {
+        GLFW.glfwInit();
+        window = GLFW.glfwCreateWindow(WIDTH, HEIGHT, "Hello LWJGL", 0, 0);
+        GLFW.glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+    }
+
+    private void loop() {
+        while (true) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            
+            GL11.glBegin(GL11.GL_TRIANGLES);
+            GL11.glVertex3f(+1.0f, 0.0f, 0.0f);
+            GL11.glVertex3f(+0.0f, 1.0f, 0.0f);
+            GL11.glVertex3f(-1.0f, 0.0f, 0.0f);
+            GL11.glEnd();
+            
+            GLFW.glfwSwapBuffers(window);
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         new HelloLWJGL();
     }
-    
+
 }
