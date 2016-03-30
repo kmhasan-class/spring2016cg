537cc11b1e368a3d4f856aed9b15b6f6d5cd27ef
kmhasan <kmhasan@SEUPC0719>
2016-03-30 09:39:03 +0600

    First LWJGL program -- lab task for section 1

--- Hello LWJGL/src/hello/lwjgl/HelloLWJGL.java
+++ Hello LWJGL/src/hello/lwjgl/HelloLWJGL.java
@@ -32,6 +32,36 @@ public class HelloLWJGL {
         GL.createCapabilities();
     }
 
+    private void drawCircle(float r, int n) {
+        // draw the dial
+        GL11.glColor3f(0.0f, 0.0f, 0.0f);
+        GL11.glPushMatrix();
+        GL11.glBegin(GL11.GL_LINES);
+        float dtheta = (float) (2 * Math.PI / n);
+        for (int i = 0; i < n; i++) {
+            GL11.glVertex3f((float) (r * Math.cos(i * dtheta)), (float) (r * Math.sin(i * dtheta)), 0f);
+            GL11.glVertex3f((float) (r * Math.cos((i + 1) * dtheta)), (float) (r * Math.sin((i + 1) * dtheta)), 0f);
+        }
+        GL11.glEnd();
+        GL11.glPopMatrix();
+    }
+
+    private void drawDottedCircle(float r, int n) {
+        // draw the dial
+        GL11.glColor3f(0.0f, 0.0f, 0.0f);
+        GL11.glPushMatrix();
+        GL11.glBegin(GL11.GL_LINES);
+        float dtheta = (float) (2 * Math.PI / n);
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                GL11.glVertex3f((float) (r * Math.cos(i * dtheta)), (float) (r * Math.sin(i * dtheta)), 0f);
+                GL11.glVertex3f((float) (r * Math.cos((i + 1) * dtheta)), (float) (r * Math.sin((i + 1) * dtheta)), 0f);
+            }
+        }
+        GL11.glEnd();
+        GL11.glPopMatrix();
+    }
+
     private void loop() {
         float angle = 0;
 
@@ -41,21 +71,23 @@ public class HelloLWJGL {
 
         float nextX, nextY;
 
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-
-        // draw the dial
-        GL11.glPushMatrix();
-        GL11.glBegin(GL11.GL_LINES);
-        int n = 100;
-        float dtheta = (float) (2 * Math.PI / n);
-        for (int i = 0; i < n; i++) {
-            GL11.glVertex3f((float) (1 * Math.cos(i * dtheta)), (float) (1 * Math.sin(i * dtheta)), 0f);
-            GL11.glVertex3f((float) (1 * Math.cos((i + 1) * dtheta)), (float) (1 * Math.sin((i + 1) * dtheta)), 0f);
-        }
-        GL11.glEnd();
-        GL11.glPopMatrix();
+        GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
         while (true) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            angle++;
+
+            drawCircle(0.25f, 5);
+            drawDottedCircle(0.5f, 50);
+            drawDottedCircle(0.8f, 50);
+            
+            GL11.glPushMatrix();
+            GL11.glRotatef(angle, 0, 0, 1);
+            GL11.glTranslatef(0.5f, 0.0f, 0.0f);
+            GL11.glRotatef(-angle * 4, 0, 0, 1);
+            drawCircle(0.1f, 5);
+            GL11.glPopMatrix();
+            /*
             double t = GLFW.glfwGetTime();
 
             // all our calculations are in radian (NOT degree)
@@ -73,7 +105,7 @@ public class HelloLWJGL {
 
             lastX = nextX;
             lastY = nextY;
-
+             */
             GLFW.glfwSwapBuffers(window);
         }
     }
